c344ccf341a8f2dda36fb1f005a99e45806e1aad
Adrian Jaworek <jawor34@gmail.com>
2022-10-10 08:44:24 +0200

    Added tile array generator

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
 onready var BoardTilesCount : int =  get_child_count()
+var BoardTiles : Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	tests()
+	if !validate_tiles_count():
+		print("Missing board! Verify BOARD object!")
+	else:
+		BoardTiles = get_all_board_tiles()
+		
+		print(BoardTiles)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-func tests():
-	if !validate_tiles_count():
-		print("Missing board! Verify BOARD object!")
 	
 func validate_tiles_count() -> bool:
 	var is_board_created : bool = true
@@ -21,4 +23,17 @@ func validate_tiles_count() -> bool:
 		is_board_created = false
 	
 	return is_board_created
-		
+
+func get_all_board_tiles() -> Array:
+	var board_tiles : Array = []
+	
+	for i in range(BoardTilesCount):
+		var tile = {
+			"TileName"	: get_child(i).name,
+			"PositionX" : get_child(i).position.x,
+			"PositionY"	: get_child(i).position.y,
+		}
+
+		board_tiles.append(tile)
+
+	return board_tiles
