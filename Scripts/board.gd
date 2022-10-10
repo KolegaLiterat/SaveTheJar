a40f7e33ff22a958152a142a7902ff33f7b0a42c
Adrian Jaworek <jawor34@gmail.com>
2022-10-10 07:55:09 +0200

    Add simple board tests

--- Scripts/board.gd
+++ Scripts/board.gd
@@ -5,14 +5,24 @@ var BoardTilesCount : int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	BoardTilesCount = get_child_count()
-
+	
+	#Test methods for board 
+	tests()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func tests():
-	pass
+	if !validate_tiles_count():
+		print("Missing board! Verify BOARD object!")
 	
 func validate_tiles_count() -> bool:
-	is_board_created = 
+	var is_board_created : bool = true
+
+	if BoardTilesCount == 0:
+		is_board_created = false
+	
+	return is_board_created
+		
