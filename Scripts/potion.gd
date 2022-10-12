359ad397327a95d34d058a312adbe491e1f3e416
Adrian Jaworek <jawor34@gmail.com>
2022-10-12 08:51:38 +0200

    Added animation handler methods

--- Scripts/potion.gd
+++ Scripts/potion.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var IsSelected : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 
 func _on_RigidBody2D_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
-	   print(self.name)
+		if IsSelected == false:
+			IsSelected = true
+			animation_handler()
+		elif IsSelected == true:
+			IsSelected = false
+			animation_handler()
+
+func animation_handler() -> void:
+	if IsSelected == true:
+		$RigidBody2D/AnimatedSprite.show()
+		$RigidBody2D/AnimatedSprite.play()
+	elif IsSelected == false:
+		$RigidBody2D/AnimatedSprite.hide()
+		$RigidBody2D/AnimatedSprite.stop()
