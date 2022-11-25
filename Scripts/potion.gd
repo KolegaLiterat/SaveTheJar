abfcaec35684adeacbb4befec23da31bb381924b
Adrian Jaworek <jawor34@gmail.com>
2022-11-25 15:00:04 +0100

    Clean up

--- Scripts/potion.gd
+++ Scripts/potion.gd
@@ -7,11 +7,10 @@ var IsRemoveable : bool = false
 var IsChosenToBeRemoved : bool = false
 var IsRotten : bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _ready() -> void:
+	pass
 
-func _process(_delta):
+func _process(_delta) -> void:
 	pass
 
 func _on_PotionBody_input_event(_viewport, event, _shape_idx) -> void:
